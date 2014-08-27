5709d713a3e194e71fb845b1297ba826ddcd73ad
Tim Smart <tim.smart@designworks.co.nz>
2014-08-27 13:50:46 +1200

    Update local-config-sample.php
    Signed-off-by: Tim Smart <tim.smart@designworks.co.nz>

--- local-config-sample.php
+++ local-config-sample.php
@@ -6,7 +6,13 @@ In it, you *must* include the four main database defines
 You may include other settings here that you only want enabled on your local development checkouts
 */
 
-define( 'DB_NAME', 'local_db_name' );
-define( 'DB_USER', 'local_db_user' );
-define( 'DB_PASSWORD', 'local_db_password' );
-define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+define( 'DB_NAME', 'meticulous' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_HOST', '192.168.1.106' ); // Probably 'localhost'
+
+define( 'WP_HOME','http://local.designworks.dev/SITE' );
+define( 'WP_SITEURL','http://local.designworks.dev/SITE/wp' ); 
+
+define( 'SAVEQUERIES', true );
+define( 'WP_DEBUG', true );
